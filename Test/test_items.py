4710b6c59115cc47d22b6c214e59a938999c5f60
e.pavlova <e.pavlova@bi.zone>
2020-11-08 19:50:56 +0300

    update test

--- Test/test_items.py
+++ Test/test_items.py
@@ -1,11 +1,16 @@
 import time
 
 
-def test_check_text_button_for_es_language(browser):
+def test_page_should_be_contain_button_add_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     #time.sleep(30)
     text_button = browser.find_element_by_css_selector("button.btn-add-to-basket").text
     print(text_button)
-    assert text_button == "Añadir al carrito", \
-            f"got {text_button} expexted 'Añadir al carrito'"
+
+    button = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+    len_element_button = len(button)
+    print(len_element_button)
+
+    assert len_element_button == 1, \
+        f" got {len_element_button} expected '1'"
